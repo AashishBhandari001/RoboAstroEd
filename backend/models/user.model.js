f6710ad9666149196b8301a74e4025b7d98c6d7a
AashishBhandari001 <np03cs4m210054@heraldcollege.edu.np>
2023-10-11 22:08:39 +0545

    Create User Model

---
+++ backend/models/user.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true, // username is required
+      unique: true, // no two users can have the same username
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+//create a model from schema
+const User = mongoose.model("User", userSchema);
+
+export default User;
