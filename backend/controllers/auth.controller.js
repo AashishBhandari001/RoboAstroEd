e4930d61a313202bdb38f02426f48b7f94883d9e
AashishBhandari001 <np03cs4m210054@heraldcollege.edu.np>
2023-10-12 22:06:35 +0545

    created middleware and function for possible error

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,15 +1,17 @@
 const User = require("../models/user.model.js");
 const bcrypt = require("bcryptjs");
+const errorHandler = require("../utils/error.js");
 
-const signup = async (req, res) => {
-  const { username, email, password } = req.body;
-  const hashedPassword = bcrypt.hashSync(password, 10);
+const signup = async (req, res, next) => {
+  const { username, email, password } = req.body; // get the username, email and password from the request body
+  const hashedPassword = bcrypt.hashSync(password, 10); // 10 is the salt (how many times the password is hashed)
   const newUser = new User({ username, email, password: hashedPassword });
   try {
+    // save the new user to the database
     await newUser.save();
     res.status(201).json({ message: "User created" });
   } catch (error) {
-    res.status(409).json({ message: error.message });
+    next(errorHandler(500, error.message));
   }
 };
 
