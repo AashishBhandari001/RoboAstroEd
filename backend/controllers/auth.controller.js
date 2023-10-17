35607fa996879d2f99a89c95a5df83aa6a05a183
AashishBhandari001 <np03cs4m210054@heraldcollege.edu.np>
2023-10-17 08:22:28 +0545

    Create sign in api route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const User = require("../models/user.model.js");
 const bcrypt = require("bcryptjs");
 const errorHandler = require("../utils/error.js");
+const jwt = require("jsonwebtoken");
 
 const signup = async (req, res, next) => {
   const { username, email, password } = req.body; // get the username, email and password from the request body
@@ -15,4 +16,22 @@ const signup = async (req, res, next) => {
   }
 };
 
-module.exports = { signup };
+const signin = async (req, res, next) => {
+  const { email, password } = req.body; // get the username, email and password from the request body
+  try {
+    const validUser = await User.findOne({ email }); // check if the user exists in the database
+    if (!validUser) return next(errorHandler(404, "User does not exist")); // if not, return an error)) {
+    const validPassword = bcrypt.compareSync(password, validUser.password); // check if the password is valid
+    if (!validPassword) return next(errorHandler(401, "Wrong Credentials")); // if not, return an error
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET); // create a token
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest); // send the token in a cookie
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { signup, signin };
