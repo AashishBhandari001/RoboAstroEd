b2d66b410ae170aeaababf23bdb38e10c4ef37b7
AashishBhandari001 <np03cs4m210054@heraldcollege.edu.np>
2023-10-31 13:12:56 +0545

    adding, updating and deleting product controller

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,7 +1,93 @@
-const  getProducts = async (req, res) => {
-  res.status(200).json({
-    message: "route is working fine",
+const e = require("express");
+const Product = require("../models/product.model");
+
+//create new product --Admin
+const createProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    product,
   });
 };
 
-module.exports = { getProducts };
+//get all products
+const getProducts = async (req, res) => {
+  const products = await Product.find();
+  res.status(200).json({
+    success: true,
+    products,
+  });
+};
+
+//get single product
+const getProductDetails = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+//update product --Admin
+const updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+//delete product --Admin
+
+const deleteProduct = async (req, res, next) => {
+  try {
+    const product = await Product.findByIdAndRemove(req.params.id);
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "Product is deleted",
+    });
+  } catch (error) {
+    // Handle errors, e.g., server error
+    return res.status(500).json({
+      success: false,
+      message: "Error deleting the product",
+      error: error.message,
+    });
+  }
+};
+
+module.exports = {
+  getProducts,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  getProductDetails,
+};
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -5,8 +5,16 @@ const router = express.Router();
 const {
   product,
   getProducts,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  getProductDetails,
 } = require("../controllers/product.controller.js");
 
 router.route("/", product).get(getProducts);
+router.route("/new", product).post(createProduct);
+
+router.route("/:id").put(updateProduct).delete(deleteProduct).get(getProductDetails);
+
 
 module.exports = router;
