cc5ed07707b7125fa95aa2ac4ea271bd75e3a300
AashishBhandari001 <np03cs4m210054@heraldcollege.edu.np>
2023-11-03 22:32:43 +0545

    Added getOrder, updateOrder and deleteOrder

--- backend/controllers/order.controller.js
+++ backend/controllers/order.controller.js
@@ -36,10 +36,8 @@ const newOrder = catchAsyncErrors(async (req, res, next) => {
 const getSingleOrder = catchAsyncErrors(async (req, res, next) => {
   const order = await Order.findById(req.params.id).populate(
     "user",
-    "name email"
+    "username email"
   ); //will get user name and email from user model using user id
-
-  console.log(order);
 
   if (!order) {
     return next(new errorHandler(404, "Order not found"));
@@ -53,8 +51,6 @@ const getSingleOrder = catchAsyncErrors(async (req, res, next) => {
 
 //get logged in user orders
 const myOrders = catchAsyncErrors(async (req, res, next) => {
-  //   console.log("myfff");
-  //   console.log(req.user.id);
   const orders = await Order.find({ user: req.user.id });
 
   return res.status(200).json({
@@ -63,4 +59,78 @@ const myOrders = catchAsyncErrors(async (req, res, next) => {
   });
 });
 
-module.exports = { newOrder, getSingleOrder, myOrders };
+//get all orders --Admin
+const getAllOrders = catchAsyncErrors(async (req, res, next) => {
+  const orders = await Order.find();
+
+  let totalAmount = 0;
+
+  orders.forEach((order) => {
+    totalAmount += order.totalPrice;
+  }); //calculate total amount of all orders
+
+  return res.status(200).json({
+    success: true,
+    totalAmount,
+    orders,
+  });
+});
+
+//update orders status --Admin
+const updateOrder = catchAsyncErrors(async (req, res, next) => {
+  const orders = await Order.findById(req.params.id);
+
+  if (!orders) {
+    return next(new errorHandler(404, "Order not found"));
+  }
+
+  if (orders.orderStatus === "Delivered") {
+    return next(errorHandler(400, "Order already delivered"));
+  }
+
+  orders.orderItems.forEach(async (o) => {
+    await updateStock(o.product, o.quantity);
+  }); //update stock
+
+  orders.orderStatus = req.body.status;
+
+  if (req.body.status === "Delivered") {
+    orders.deliveredAt = Date.now();
+  }
+
+  await orders.save({ validateBeforeSave: false });
+
+  return res.status(200).json({
+    success: true,
+  });
+});
+
+async function updateStock(id, quantity) {
+  const product = await Product.findById(id);
+
+  product.stock -= quantity;
+
+  await product.save({ validateBeforeSave: false });
+}
+
+//delete order --Admin
+const deleteOrder = catchAsyncErrors(async (req, res, next) => {
+  const orders = await Order.findByIdAndRemove(req.params.id);
+
+  if (!orders) {
+    return next(new errorHandler(404, "Order not found"));
+  }
+
+  return res.status(200).json({
+    success: true,
+  });
+});
+
+module.exports = {
+  newOrder,
+  getSingleOrder,
+  myOrders,
+  getAllOrders,
+  updateOrder,
+  deleteOrder,
+};
--- backend/routes/order.route.js
+++ backend/routes/order.route.js
@@ -8,6 +8,9 @@ const {
   newOrder,
   getSingleOrder,
   myOrders,
+  getAllOrders,
+  updateOrder,
+  deleteOrder,
 } = require("../controllers/order.controller.js");
 
 const router = express.Router();
@@ -15,8 +18,15 @@ const router = express.Router();
 router.route("/new").post(isAuthenticatedUser, newOrder);
 router.route("/me").get(isAuthenticatedUser, myOrders);
 
+router.route("/:id").get(isAuthenticatedUser, getSingleOrder);
+
 router
-  .route("/:id")
-  .get(isAuthenticatedUser, authorizeRoles("admin"), getSingleOrder);
+  .route("/admin/orders")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getAllOrders);
+
+router
+  .route("/admin/orders/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateOrder)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteOrder);
 
 module.exports = router;
