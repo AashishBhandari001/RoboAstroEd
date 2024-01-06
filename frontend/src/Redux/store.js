78caaf02578d9b6c9a65cba1845006a00c698ee9
Aashish <np03cs4m210054@heraldcollege.edu.np>
2024-01-06 18:56:32 +0545

    feat: Implement Redux for admin user actions

--- frontend/src/Redux/store.js
+++ frontend/src/Redux/store.js
@@ -15,22 +15,32 @@ import {
   adminReducer,
   courseReducer,
   courseLectureReducer,
+  addLectureReducer,
 } from "../Reducers/courseReducer";
 import userReducer from "./user/userSlice";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { cartReducer } from "../Reducers/cartReducer";
+import {
+  allUsersReducer,
+  userDetailsReducer,
+  userUpdateDeleteReducer,
+} from "../Reducers/userReducer";
 
 const rootReducer = combineReducers({
   products: ProductReducers,
   productDetails: productDetailsReducers,
   user: userReducer,
+  allUser: allUsersReducer, //all user reducer for admin
+  userDetails: userDetailsReducer, //user details reducer for admin
+  userUpdateDelete: userUpdateDeleteReducer, //update and delete user reducer for admin
   cart: cartReducer,
   newProduct: newProductReducer,
   product: productReduser,
   courses: courseReducer,
   lectures: courseLectureReducer,
   admin: adminReducer,
+  addlecture: addLectureReducer,
 });
 
 const persistConfig = {
