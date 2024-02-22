6c61b1dfcc76d35f0180035c57cfedf22e03357b
Aashish <np03cs4m210054@heraldcollege.edu.np>
2024-02-22 22:42:11 +0545

    refactor: added token in google sign in

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -142,11 +142,7 @@ const google = async (req, res, next) => {
     const user = await User.findOne({ email: req.body.email }); // check if the user exists in the database
     if (user) {
       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET); // create a token
-      const { password: pass, ...rest } = user._doc;
-      res
-        .cookie("access_token", token, { httpOnly: true })
-        .status(200)
-        .json(rest);
+      res.status(200).json({ token, ...user._doc }); // Send token and user data in response
     } else {
       const generatedPassword =
         Math.random().toString(36).slice(-8) +
@@ -158,15 +154,11 @@ const google = async (req, res, next) => {
           Math.random().toString(36).slice(-4),
         email: req.body.email,
         password: hashedPassword,
-        verified: true,
+        verified: true, // Set verified to true for Google sign-up
       });
       await newUser.save();
       const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET); // create a token
-      const { password: pass, ...rest } = newUser._doc;
-      res
-        .cookie("access_token", token, { httpOnly: true })
-        .status(200)
-        .json(rest); // send the token in a cookie
+      res.status(200).json({ token, ...newUser._doc }); // Send token and new user data in response
     }
   } catch (error) {
     next(error);
